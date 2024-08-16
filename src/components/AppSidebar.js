d82905ba7df41090356a498553c3f25003266d20
Duy <voanduy1822001@gmail.com>
2024-08-16 16:24:13 +0700

    Base "Fix Menu, Routes, Folder"

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -4,18 +4,15 @@ import { useSelector, useDispatch } from 'react-redux'
 import {
   CCloseButton,
   CSidebar,
-  CSidebarBrand,
   CSidebarFooter,
   CSidebarHeader,
   CSidebarToggler,
 } from '@coreui/react'
-import CIcon from '@coreui/icons-react'
 
 import { AppSidebarNav } from './AppSidebarNav'
 
-import { logo } from 'src/assets/brand/logo'
-import { sygnet } from 'src/assets/brand/sygnet'
-
+import logo from 'src/assets/brand/Logo.png'
+import { CAvatar } from '@coreui/react'
 // sidebar nav config
 import navigation from '../_nav'
 
@@ -36,10 +33,8 @@ const AppSidebar = () => {
       }}
     >
       <CSidebarHeader className="border-bottom">
-        <CSidebarBrand to="/">
-          <CIcon customClassName="sidebar-brand-full" icon={logo} height={32} />
-          <CIcon customClassName="sidebar-brand-narrow" icon={sygnet} height={32} />
-        </CSidebarBrand>
+      <div className="card-title"><h5> GĐPT LINH SƠN</h5></div>
+        <CAvatar src={logo} />
         <CCloseButton
           className="d-lg-none"
           dark
