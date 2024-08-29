ee807531dc9fbf0db053bd93b6698284b9fd099c
khoa <128579628+vakhoa4875@users.noreply.github.com>
2024-08-29 13:17:46 +0700

    LS018: full authorization for all logged in account

--- src/_nav.js
+++ src/_nav.js
@@ -1,63 +1,69 @@
 import {
+  cilBell,
+  cilBook,
   cilFile,
   cilGroup,
   cilHouse,
   cilMoney,
   cilPlus,
   cilUserPlus,
-  cilBell,
-  cilBook,
 } from '@coreui/icons'
 import CIcon from '@coreui/icons-react'
 import { CNavGroup, CNavItem, CNavTitle } from '@coreui/react'
 import React from 'react'
-import { Role } from './GlobalVariable'
-
 const _nav = [
   {
     component: CNavItem,
     name: 'Trang Chủ',
     to: '/',
-    icon: <CIcon icon={cilHouse} customClassName="nav-icon" />,
+    icon: <CIcon icon={cilHouse} customClassName='nav-icon' />,
+    screen_id: 'trang-chu'
   },
   {
     component: CNavTitle,
     name: 'Quản Lý',
-    role: [Role.ROLE_DOANTRUONG_NGANHTHANH, Role.ROLE_DOANTRUONG_OANHVUNAM, Role.ROLE_DOANTRUONG_OANHVUNU, Role.ROLE_DOANTRUONG_THIEUNAM, Role.ROLE_DOANTRUONG_THIEUNU]
+    // role: [Role.ROLE_DOANTRUONG_NGANHTHANH, Role.ROLE_DOANTRUONG_OANHVUNAM, Role.ROLE_DOANTRUONG_OANHVUNU, Role.ROLE_DOANTRUONG_THIEUNAM, Role.ROLE_DOANTRUONG_THIEUNU],
+    screen_id: 'quan-ly'
   },
   {
     component: CNavGroup,
     name: 'Danh Sách Đoàn Sinh',
     to: '/doan-sinh',
-    icon: <CIcon icon={cilGroup} customClassName="nav-icon" />,
-    role: [Role.ROLE_DOANTRUONG_NGANHTHANH, Role.ROLE_DOANTRUONG_OANHVUNAM, Role.ROLE_DOANTRUONG_OANHVUNU, Role.ROLE_DOANTRUONG_THIEUNAM, Role.ROLE_DOANTRUONG_THIEUNU],
+    icon: <CIcon icon={cilGroup} customClassName='nav-icon' />,
+    screen_id: 'doan-sinh',
+    // role: [Role.ROLE_DOANTRUONG_NGANHTHANH, Role.ROLE_DOANTRUONG_OANHVUNAM, Role.ROLE_DOANTRUONG_OANHVUNU, Role.ROLE_DOANTRUONG_THIEUNAM, Role.ROLE_DOANTRUONG_THIEUNU],
     items: [
       {
         component: CNavGroup,
         name: 'Đoàn Thiếu Nam',
         to: '/ds-doan-sinh',
-        icon: <CIcon icon={cilPlus} customClassName="nav-icon" />,
-        role: [Role.ROLE_DOANTRUONG_THIEUNAM],
+        icon: <CIcon icon={cilPlus} customClassName='nav-icon' />,
+        // role: [Role.ROLE_DOANTRUONG_THIEUNAM],
+        screen_id: 'doan-sinh.doan-thieu-nam',
         items: [
           {
             component: CNavItem,
             name: 'Danh Sách',
             to: '/doan-sinh/ds-thieu-nam',
+            screen_id: 'doan-sinh.doan-thieu-nam.danh-sach'
           },
           {
             component: CNavItem,
             name: 'Điểm Danh',
             to: '/doan-sinh/dd-thieu-nam',
+            screen_id: 'doan-sinh.doan-thieu-nam.dd-danh'
           },
           {
             component: CNavItem,
             name: 'Quỹ Đoàn',
             to: '/doan-sinh/qd-thieu-nam',
+            screen_id: 'doan-sinh.doan-thieu-nam.qd-doan'
           },
           {
             component: CNavItem,
             name: 'Đoàn Phả',
             to: '/doan-sinh/dp-thieu-nam',
+            screen_id: 'doan-sinh.doan-thieu-nam.dp-doan'
           }
         ],
       },
@@ -65,28 +71,33 @@ const _nav = [
         component: CNavGroup,
         name: 'Đoàn Thiếu Nữ',
         to: '/ds-doan-sinh',
-        icon: <CIcon icon={cilPlus} customClassName="nav-icon" />,
-        role: [Role.ROLE_DOANTRUONG_THIEUNU],
+        icon: <CIcon icon={cilPlus} customClassName='nav-icon' />,
+        // role: [Role.ROLE_DOANTRUONG_THIEUNU],
+        screen_id: 'doan-sinh.doan-thieu-nu',
         items: [
           {
             component: CNavItem,
             name: 'Danh Sách',
             to: '/doan-sinh/ds-thieu-nu',
+            screen_id: 'doan-sinh.doan-thieu-nu.danh-sach'
           },
           {
             component: CNavItem,
             name: 'Điểm Danh',
             to: '/doan-sinh/dd-thieu-nu',
+            screen_id: 'doan-sinh.doan-thieu-nu.dd-danh'
           },
           {
             component: CNavItem,
             name: 'Quỹ Đoàn',
             to: '/doan-sinh/qd-thieu-nu',
+            screen_id: 'doan-sinh.doan-thieu-nu.qd-doan'
           },
           {
             component: CNavItem,
             name: 'Đoàn Phả',
             to: '/doan-sinh/dp-thieu-nu',
+            screen_id: 'doan-sinh.doan-thieu-nu.dp-doan'
           }
         ],
       },
@@ -94,28 +105,33 @@ const _nav = [
         component: CNavGroup,
         name: 'Đoàn Oanh Vũ Nữ',
         to: '/ds-doan-sinh',
-        icon: <CIcon icon={cilPlus} customClassName="nav-icon" />,
-        role: [Role.ROLE_DOANTRUONG_OANHVUNU],
+        icon: <CIcon icon={cilPlus} customClassName='nav-icon' />,
+        // role: [Role.ROLE_DOANTRUONG_OANHVUNU],
+        screen_id: 'doan-sinh.doan-oanh-vu-nu',
         items: [
           {
             component: CNavItem,
             name: 'Danh Sách',
             to: '/doan-sinh/ds-oanh-vu-nu',
+            screen_id: 'doan-sinh.doan-oanh-vu-nu.danh-sach'
           },
           {
             component: CNavItem,
             name: 'Điểm Danh',
             to: '/doan-sinh/dd-oanh-vu-nu',
+            screen_id: 'doan-sinh.doan-oanh-vu-nu.dd-danh'
           },
           {
             component: CNavItem,
             name: 'Quỹ Đoàn',
             to: '/doan-sinh/qd-oanh-vu-nu',
+            screen_id: 'doan-sinh.doan-oanh-vu-nu.qd-doan'
           },
           {
             component: CNavItem,
             name: 'Đoàn Phả',
             to: '/doan-sinh/dp-oanh-vu-nu',
+            screen_id: 'doan-sinh.doan-oanh-vu-nu.dp-doan'
           }
         ],
       },
@@ -123,28 +139,33 @@ const _nav = [
         component: CNavGroup,
         name: 'Đoàn Oanh Vũ Nam',
         to: '/ds-doan-sinh',
-        icon: <CIcon icon={cilPlus} customClassName="nav-icon" />,
-        role: [Role.ROLE_DOANTRUONG_OANHVUNAM],
+        icon: <CIcon icon={cilPlus} customClassName='nav-icon' />,
+        // role: [Role.ROLE_DOANTRUONG_OANHVUNAM],
+        screen_id: 'doan-sinh.doan-oanh-vu-nam',
         items: [
           {
             component: CNavItem,
             name: 'Danh Sách',
             to: '/doan-sinh/ds-oanh-vu-nam',
+            screen_id: 'doan-sinh.doan-oanh-vu-nam.danh-sach'
           },
           {
             component: CNavItem,
             name: 'Điểm Danh',
             to: '/doan-sinh/dd-oanh-vu-nam',
+            screen_id: 'doan-sinh.doan-oanh-vu-nam.dd-danh'
           },
           {
             component: CNavItem,
             name: 'Quỹ Đoàn',
             to: '/doan-sinh/qd-oanh-vu-nam',
+            screen_id: 'doan-sinh.doan-oanh-vu-nam.qd-doan'
           },
           {
             component: CNavItem,
             name: 'Đoàn Phả',
             to: '/doan-sinh/dp-oanh-vu-nam',
+            screen_id: 'doan-sinh.doan-oanh-vu-nam.dp-doan'
           }
         ],
       },
@@ -152,56 +173,63 @@ const _nav = [
         component: CNavGroup,
         name: 'Ngành Thanh',
         to: '/ds-doan-sinh',
-        icon: <CIcon icon={cilPlus} customClassName="nav-icon" />,
-        role: [Role.ROLE_DOANTRUONG_NGANHTHANH],
+        icon: <CIcon icon={cilPlus} customClassName='nav-icon' />,
+        // role: [Role.ROLE_DOANTRUONG_NGANHTHANH],
+        screen_id: 'doan-sinh.nganh-thanh',
         items: [
           {
             component: CNavItem,
             name: 'Danh Sách',
             to: '/doan-sinh/ds-nganh-thanh',
+            screen_id: 'doan-sinh.nganh-thanh.danh-sach'
           },
           {
             component: CNavItem,
             name: 'Điểm Danh',
             to: '/doan-sinh/dd-nganh-thanh',
+            screen_id: 'doan-sinh.nganh-thanh.dd-danh'
           },
           {
             component: CNavItem,
             name: 'Quỹ Đoàn',
             to: '/doan-sinh/qd-nganh-thanh',
+            screen_id: 'doan-sinh.nganh-thanh.qd-doan'
           },
           {
             component: CNavItem,
             name: 'Đoàn Phả',
             to: '/doan-sinh/dp-nganh-thanh',
+            screen_id: 'doan-sinh.nganh-thanh.dp-doan'
           }
         ],
       },
     ],
   },
-
-
   {
     component: CNavGroup,
     name: 'Huynh Trưởng',
     to: '/huynh-truong',
-    icon: <CIcon icon={cilUserPlus} customClassName="nav-icon" />,
-    role: [Role.ROLE_ADMIN],
+    icon: <CIcon icon={cilUserPlus} customClassName='nav-icon' />,
+    // role: [Role.ROLE_ADMIN],
+    screen_id: 'huynh-truong',
     items: [
       {
         component: CNavItem,
         name: 'Danh sách',
         to: '/huynh-truong/danh-sach',
+        screen_id: 'huynh-truong.danh-sach'
       },
       {
         component: CNavItem,
         name: 'Tài Khoản',
         to: '/huynh-truong/tai-khoan',
+        screen_id: 'huynh-truong.tai-khoan'
       },
       {
         component: CNavItem,
         name: 'Chức Vụ',
-        to: '/chuc-vu'
+        to: '/chuc-vu',
+        screen_id: 'chuc-vu'
       },
     ],
   },
@@ -209,41 +237,45 @@ const _nav = [
     component: CNavItem,
     name: 'Quỹ Gia Đình',
     to: '/quygd',
-    icon: <CIcon icon={cilMoney} customClassName="nav-icon" />,
-    role: [Role.ROLE_THUQUY]
+    icon: <CIcon icon={cilMoney} customClassName='nav-icon' />,
+    // role: [Role.ROLE_THUQUY],
+    screen_id: 'quy-gia-dinh'
   },
   {
     component: CNavItem,
     name: 'Bậc Học',
     to: '/bac-hoc',
-    icon: <CIcon icon={cilBook} customClassName="nav-icon" />,
-    role: [Role.ROLE_THUQUY]
+    icon: <CIcon icon={cilBook} customClassName='nav-icon' />,
+    // role: [Role.ROLE_THUQUY],
+    screen_id: 'bac-hoc'
   },
   {
     component: CNavTitle,
     name: 'Tài Liệu',
-    role: [Role.ROLE_THUKY]
+    // role: [Role.ROLE_THUKY],
+    screen_id: 'tai-lieu'
   },
   {
     component: CNavItem,
     name: 'File Lưu Trữ',
     to: '/file-luu-tru',
-    icon: <CIcon icon={cilFile} customClassName="nav-icon" />
-    
+    icon: <CIcon icon={cilFile} customClassName='nav-icon' />,
+    screen_id: 'file-luu-tru'
   },
   {
     component: CNavTitle,
     name: 'Hệ Thống',
-    role: [Role.ROLE_THUKY]
+    // role: [Role.ROLE_THUKY],
+    screen_id: 'he-thong'
   },
   {
     component: CNavItem,
     name: 'Thông Báo',
     to: '/thong-bao',
-    icon: <CIcon icon={cilBell} customClassName="nav-icon" />
+    icon: <CIcon icon={cilBell} customClassName='nav-icon' />,
+    screen_id: 'thong-bao'
   },
+];
 
-
-]
 
 export default _nav
