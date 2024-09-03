40d84c1558f80cddb6794866a14735f2925311db
khoa <128579628+vakhoa4875@users.noreply.github.com>
2024-09-03 22:35:46 +0700

    LS018: add axios interceptors, handle api error calls

---
+++ src/apiClient.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import Swal from 'sweetalert2';
+import env from './env';
+
+const apiClient = axios.create({
+  baseURL: env.apiUrl
+});
+
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = sessionStorage.getItem('token');
+    console.log(token);
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+apiClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 403) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Đã có người đăng nhập vào tài khoản này!'
+      }).then(() => {
+        sessionStorage.clear();
+        window.location.href('/login');
+      })
+    } else if (error.response) {
+      Swal.fire({
+        icon: 'warning',
+        title: error.response?.data?.message || 'Thao tác dữ liệu thất bại!'
+      })
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
