cd6419b8b07a8ece19b0e3a8b1e8dfae0571f1f3
khoa <128579628+vakhoa4875@users.noreply.github.com>
2024-09-23 19:05:04 +0700

    feat: change userAvatarUrl

--- src/env.js
+++ src/env.js
@@ -2,7 +2,7 @@ const env = {
     // apiUrl: 'http://localhost:8080',
     apiUrl: 'http://183.80.113.184:6868',
     // apiUrl: 'http://103.15.222.65:8888',
-    userAvatarUrl: 'http://103.15.222.65/images/'
+    userAvatarUrl: 'http://183.80.113.184:8888/images/'
 };
 
 export default env;
